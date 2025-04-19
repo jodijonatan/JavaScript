fe5f11f9cdff4acd441ede95ed434024eb379082
jodijonatan <jdi102350@gmail.com>
2025-04-19 17:19:52 +0700

    add days and update loop

---
+++ day.js
@@ -0,0 +1,6 @@
+let today = new Date();
+let dayNumber = today.getDay();
+
+const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+
+console.log(`Hari ini adalah ${days[dayNumber]}`);
--- loop.js
+++ loop.js
@@ -1,3 +1,8 @@
-for (i = 1; i <= 100; i++) {
-  console.log(`kontol ke-${i}`);
+// for (i = 1; i <= 100; i++) {
+//   console.log(`kontol ke-${i}`);
+// }
+
+let numbers = [100, 200, 300, 400];
+for (const number of numbers) {
+  console.log(number);
 }
