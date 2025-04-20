7d744ecda7533cbcaa61018a5d75433c636ada70
jodijonatan <jdi102350@gmail.com>
2025-04-20 16:16:14 +0700

    learn js dom pt2

--- jsDOM/main.js
+++ jsDOM/main.js
@@ -12,10 +12,12 @@ btn1.style.background = "blue";
 btn1.style.marginRight = "15px";
 btn1.style.border = "none";
 btn1.style.padding = "10px 20px";
+btn1.style.borderRadius = "100px";
 
 btn2.style.background = "red";
 btn2.style.border = "none";
 btn2.style.padding = "10px 20px";
+btn2.style.borderRadius = "100px";
 
 function klik() {
   alert("BERHASIL DIKLIK");
@@ -30,8 +32,16 @@ function keluar() {
   btn1.style.background = "blue";
 }
 
-const nama = document.createElement("h1");
+const body = document.body;
+let nama = null;
 
 function masuk2() {
-  nama.textContent = "Jodi Jonatan";
+  nama = document.createElement("h3");
+  nama.textContent = "JODI JONATAN";
+  body.append(nama);
+}
+
+function keluar2() {
+  nama.style.color = "blue";
+  body.append(nama);
 }
