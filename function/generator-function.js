a3206c3f1abdaa1ce2a6d868546e8a6052691136
Jodi Jonatan <jdi102350@gmail.com>
2025-07-11 17:36:17 +0700

    learn arrow function p1

--- function/generator-function.js
+++ function/generator-function.js
@@ -8,3 +8,16 @@ const names = createNames();
 for (const name of names) {
   console.log(name);
 }
+
+function* buatGanjil(value) {
+  for (i = 0; i <= value; i++) {
+    if (i % 2 === 1) {
+      yield i;
+    }
+  }
+}
+
+const numbers = buatGanjil(100);
+for (const number of numbers) {
+  console.log(number);
+}
