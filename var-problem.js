a9a320a6d2a584627689381816ed0c9f10ec5390
Jodi Jonatan <jdi102350@gmail.com>
2025-07-13 15:37:27 +0700

    learn problem variable var

---
+++ var-problem.js
@@ -0,0 +1,21 @@
+// dengan variable let
+// global scope
+// let i = 20;
+
+// for (let i = 0; i <= 10; i++) {
+//   // local scope
+//   console.log(`local ${i}`);
+// }
+
+// console.log(`global ${i}`);
+
+// dengan variable var
+// global scope
+var j = 20;
+
+for (var j = 0; j <= 10; j++) {
+  // local scope
+  console.log(`local ${j}`);
+}
+
+console.log(`global ${j}`);
