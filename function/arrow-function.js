a66da2d609d368080f247e9953a7e446bc5768d6
Jodi Jonatan <jdi102350@gmail.com>
2025-07-13 09:58:30 +0700

    learn arrow function

--- function/arrow-function.js
+++ function/arrow-function.js
@@ -1,27 +1,28 @@
 const arrow = (firstName, lastName) => {
   console.log(`Hello ${firstName} ${lastName}`);
 };
-
 arrow("Jodi", "Jonatan");
 
 // arrow function tanpa block
 const withoutBlock = (a, b) => console.log(`${a} + ${b} = ${a + b}`);
-
 withoutBlock(10, 10);
 
 // arrow function dengan block, perlu gunakan keyword return
 const sum1 = (first, second) => {
   return first + second;
 };
-
 console.log(sum1(10, 10));
 
 // arrow function tanpa block, tidak perlu gunakan keyword return
 const sum2 = (first, second) => first + second;
-
 console.log(sum2(10, 10));
 
 // jika parameter pada arrow function hanya satu, kita bisa tidak menggunakan kurung pada parameter
 const sayHello = (name) => console.log(`Hello ${name}`);
-
 sayHello("Jonatan");
+
+// arrow function sebagai parameter
+function giveMeName(callback) {
+  callback("Jodi");
+}
+giveMeName((name) => console.log(`Hello ${name}`));
